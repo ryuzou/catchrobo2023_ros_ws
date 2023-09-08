38e10e386579a0062a9e92a30c4a2bc2cdbe506c
Yoshimura2771 <y.y.cricket@outlook.jp>
2023-09-08 13:55:43 +0900

    recreate the repository

---
+++ src/inverse_kinematics_node.cpp
@@ -0,0 +1,41 @@
+#include <cstdio>
+#include "rclcpp/rclcpp.hpp"
+#include <cmath>
+
+#include <memory>
+
+#include "robot_kinematics.h"
+
+
+
+using namespace std;
+
+void calc_inverse_kinematics(const std::shared_ptr<	> request, std::shared_ptr<	> response)
+{
+	robot_kinamatics robot_kinematics;
+
+	
+	robot_kinematics.inverse_kinematics(,);
+
+
+
+	RCLCPP_INFO(rclcpp::get_logger("rclcpp"),"");
+	RCLCPP_INFO(rclcpp::get_logger("rclcpp"),"sending back response:");
+
+}
+
+int main(int argc, char ** argv)
+{
+  (void) argc;
+  (void) argv;
+
+  rclcpp::init(argc, arg);
+
+  std::shared_ptr<rclcpp::Node> node = rclcpp::Node::make_shared("inverse_kinematics_server");
+
+  rclcpp::Service<	>::SharedPtr service = node->create_service<	>("inverse_kinematics");
+  RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Inverse kinematics ready");
+
+  rclcpp::spin(node);
+
+}
